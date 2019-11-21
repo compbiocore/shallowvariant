2f922d0320e164752f5af7ee227dcd77693f0779
mcmcgrath13 <m.c.mcgrath13@gmail.com>
2019-11-21 01:06:04 -0500

    docs: add instructions to run docker container

--- lib/get_probs.py
+++ lib/get_probs.py
@@ -1,3 +1,12 @@
+"""
+This file is copied into the docker image, where it can be used to convert the outputs of call_variants to a tsv of probabilitites.
+
+From /shallowvariant:
+docker build -t get_probs .
+docker run -it -v `pwd`:/input get_probs
+python get_probs.py -i /input/data/labels/<infile> -o /input/data/labels/<outfile>
+"""
+
 from deepvariant.protos import deepvariant_pb2
 from third_party.nucleus.util import io_utils
 import argparse
@@ -7,13 +16,11 @@ parser.add_argument("--infile", "-i")
 parser.add_argument("--outfile", "-o")
 
 def read_write_call_variant_output(infile, outfile):
-    probs = []
-    for record in io_utils.read_tfrecords(infile, deepvariant_pb2.CallVariantsOutput):
-        probs.append(record.genotype_probabilities)
-
     f = open(outfile, "w")
-
-    f.write("\t".join(probs))
+    for record in io_utils.read_tfrecords(infile, deepvariant_pb2.CallVariantsOutput):
+        probs = list(record.genotype_probabilities)
+        f.write("\t".join(map(str, probs)))
+        f.write("\n")
 
     f.close()
 
